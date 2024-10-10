5faed385ef04d7d7d93b90f28dfc3492e4843fdc
Aliyan15 <aliyanamir15@gmail.com>
2024-10-10 16:20:31 -0700

    First Commit

---
+++ supportyio-page/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Headerbuttons from './components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className='container'>
+      <Headerbuttons></Headerbuttons>
+    </div>
+  );
+}
+
+export default App;
---
+++ supportyio-page/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./header.css";
+import { useState } from "react";
+
+export default function Headerbuttons() {
+
+    const [selected, setselected] = useState("Template");
+
+    return (
+        <div className="container d-flex gap-2 justify-content-center py-5">
+            <div className="responsive">
+                <button className={`button2 rounded-pill px-3 m-2 ${selected === 'Template' && "selected"}`} onClick={() => setselected("Template")}>All Templates</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Education' && "selected"}`} type="button" onClick={() => setselected("Education")}>Education</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Support' && "selected"}`} type="button" onClick={() => setselected("Support")}>Support</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Email' && "selected"}`} type="button" onClick={() => setselected("Email")}>Email</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Language' && "selected"}`} type="button" onClick={() => setselected("Language")}>Language</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Writing Content' && "selected"}`} type="button" onClick={() => setselected("Writing Content")}>Writing Content</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Other' && "selected"}`} type="button" onClick={() => setselected("Other")}>Other</button>
+                <button className={`button2 rounded-pill px-3  m-2 ${selected === 'Favourites' && "selected"}`} type="button" onClick={() => setselected("Favourites")}>Favourites</button>
+            </div>
+        </div>
+    )
+};
