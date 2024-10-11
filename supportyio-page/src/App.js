2bd57ee790fafe67a904ba686113160c94041d7b
Aliyan15 <aliyanamir15@gmail.com>
2024-10-11 16:00:07 -0700

    card

--- supportyio-page/src/App.js
+++ supportyio-page/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import Post from './components/Card';
 import Headerbuttons from './components/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className='container'>
-      <Headerbuttons></Headerbuttons>
-    </div>
+    <>
+      <div className='container' >
+        <Headerbuttons></Headerbuttons>
+      </div>
+      <div className='container-fluid' style={{marginLeft:"50px"}}>
+        <div className='d-flex' style={{flexWrap:"wrap"}}>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          <Post></Post>
+          
+        </div>
+        
+      </div>
+    </>
+    
+    
   );
 }
 
---
+++ supportyio-page/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Post() {
+    return (
+        <div className="card m-2" style={{width: "18rem"}}>
+            <div className="card-body">
+                <h5 className="card-title m-2">AIDA Framework</h5>
+                <p className="card-text">Impress POTENTIAL EMPLOYERS WITH JOB APPLICATION EMAILS THAT STANDOUT FROM THE COMPETITION.</p>
+            </div>
+        </div>       
+    )
+}
